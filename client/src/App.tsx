7c57b9fc0e1028c025af5ee2dc40e5efb43536ab
Marinize Santana <marinize.santana.dev@gmail.com>
2026-08-19 15:34:40 -0300

    fix: configura base do roteador para GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,20 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router as WouterRouter, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/404" component={NotFound} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/portfolio">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/404" component={NotFound} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
@@ -29,4 +31,3 @@ export default function App() {
     </ErrorBoundary>
   );
 }
-
